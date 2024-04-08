7ba92e118f357e0d9cfe9d662e20eba8cd790939
Jonathan Mooney <moonejon@gmail.com>
2024-04-08 15:48:11 -0500

    login boilerplate

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div className="login">
-            {/* Your login form goes here */}
-        </div>
-    );
+  const login = () => {};
+  return (
+    <div className="login">
+      <label> Login</label>
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(event) => {
+          setUser({ ...user, username: event.target.value });
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(event) => {
+          setUser({ ...user, password: event.target.value });
+        }}
+      />
+      <button onSubmit={login}>Login</button>
+    </div>
+  );
 };
 
 export default Login;
