65ec5b22b7b26e81d7dffa67990674b16eb1eb7e
Jonathan Mooney <moonejon@gmail.com>
2024-04-09 10:57:28 -0500

    adding log out button

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
-import "./App.css";
+import React, { useState } from "react";
 import Cookies from "universal-cookie";
 import { StreamChat } from "stream-chat";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import "./App.css";
 
 function App() {
-
   const cookies = new Cookies();
   const token = cookies.get("token");
   const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
 
   const [isAuth, setIsAuth] = useState(false);
+
+  const logOut = () => {
+    cookies.remove("token");
+    cookies.remove("userId");
+    cookies.remove("firstName");
+    cookies.remove("lastName");
+    cookies.remove("username");
+    cookies.remove("hashedPassword");
+    client.disconnectUser();
+    setIsAuth(false);
+  };
 
   if (token) {
     client
@@ -30,11 +41,15 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Authentication</h1>
-      <div className="auth">
-        <SignUp setIsAuth={setIsAuth}/>
-        <Login setIsAuth={setIsAuth}/>
-      </div>
+      {isAuth ? (
+        <button onClick={logOut}>Log Out</button>
+      ) : (
+        <div className="auth">
+          {isAuth && <h2>Authenticated</h2>}
+          <SignUp setIsAuth={setIsAuth} />
+          <Login setIsAuth={setIsAuth} />
+        </div>
+      )}
     </div>
   );
 }
