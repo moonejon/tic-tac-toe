64bbe1684b55cf28867caff0d50e987bf36783a9
Jonathan Mooney <moonejon@gmail.com>
2024-04-14 23:27:31 -0500

    fixing log out modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,88 @@
-import React, { useState } from "react";
-import Cookies from "universal-cookie";
-import { StreamChat } from "stream-chat";
-import { Chat } from "stream-chat-react";
-import Game from "./components/Game";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import "./App.css";
-import JoinGame from "./components/JoinGame";
-import { Button } from "@mui/material";
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Game from './components/Game';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import './App.css';
+import JoinGame from './components/JoinGame';
+import { Button, Dialog, DialogActions, DialogTitle } from '@mui/material';
 
 function App() {
   const cookies = new Cookies();
-  const token = cookies.get("token");
+  const token = cookies.get('token');
   const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
-  
+
   const [isAuth, setIsAuth] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const logOut = () => {
-    cookies.remove("token");
-    cookies.remove("userId");
-    cookies.remove("firstName");
-    cookies.remove("lastName");
-    cookies.remove("username");
-    cookies.remove("hashedPassword");
+    cookies.remove('token');
+    cookies.remove('userId');
+    cookies.remove('firstName');
+    cookies.remove('lastName');
+    cookies.remove('username');
+    cookies.remove('hashedPassword');
     client.disconnectUser();
     setIsAuth(false);
+    setOpenDialog(false);  // Close dialog after logout
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
   };
 
   if (token) {
-    client
-      .connectUser(
-        {
-          id: cookies.get("userId"),
-          name: cookies.get("username"),
-          firstName: cookies.get("firstName"),
-          lastName: cookies.get("lastName"),
-          hashedPassword: cookies.get("hashedPassword"),
-        },
-        token
-      )
-      .then(() => {
-        setIsAuth(true);
-      });
+    client.connectUser(
+      {
+        id: cookies.get('userId'),
+        name: cookies.get('username'),
+        firstName: cookies.get('firstName'),
+        lastName: cookies.get('lastName'),
+        hashedPassword: cookies.get('hashedPassword'),
+      },
+      token
+    ).then(() => {
+      setIsAuth(true);
+    });
   }
-  
-  const username = cookies.get("username");
+
+  const username = cookies.get('username');
 
   return (
     <div className="App">
       <div className="title text-pop-up-top">Tic Tac Toe</div>
       {isAuth ? (
         <>
-          <div className="username-display">{username}</div>
+          <div className="username-display" style={{ cursor: 'pointer', transition: 'color 0.3s' }} onMouseOver={e => e.target.style.color = '#3f51b5'} onMouseOut={e => e.target.style.color = 'black'} onClick={handleOpenDialog}>
+            {username}
+          </div>
           <Chat client={client}>
             <JoinGame />
-            <button onClick={logOut}>Log Out</button>
           </Chat>
+          <Dialog open={openDialog} onClose={handleCloseDialog}>
+            <DialogTitle>Are you sure you want to log out?</DialogTitle>
+            <DialogActions>
+              <Button onClick={handleCloseDialog}>Cancel</Button>
+              <Button onClick={logOut} color="primary" autoFocus>
+                Log Out
+              </Button>
+            </DialogActions>
+          </Dialog>
         </>
       ) : (
         <div className="auth">
           <div
             style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              gap: "20px",
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: '20px',
             }}
           >
             <SignUp setIsAuth={setIsAuth} />
