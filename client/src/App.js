477ecc968cb158d766525781d47832be3a8a0017
Jonathan Mooney <moonejon@gmail.com>
2024-04-14 23:15:28 -0500

    improving app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from "universal-cookie";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
-import { Modal, Button, Box } from "@mui/material";
 import Game from "./components/Game";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import "./App.css";
 import JoinGame from "./components/JoinGame";
+import { Button } from "@mui/material";
+
+const cookies = new Cookies();
+
+// Helper functions for cookies
+function getUserFromCookies() {
+  return {
+    id: cookies.get("userId"),
+    name: cookies.get("username"),
+    firstName: cookies.get("firstName"),
+    lastName: cookies.get("lastName"),
+    hashedPassword: cookies.get("hashedPassword"),
+    token: cookies.get("token"),
+  };
+}
+
+function clearUserCookies() {
+  ["token", "userId", "firstName", "lastName", "username", "hashedPassword"].forEach(cookie => cookies.remove(cookie));
+}
 
 function App() {
-  const cookies = new Cookies();
-  const token = cookies.get("token");
-  const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
-
   const [isAuth, setIsAuth] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
+  const { token, ...user } = getUserFromCookies();
 
-  const handleOpenModal = () => {
-    setOpenModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
+  useEffect(() => {
+    if (token) {
+      client.connectUser(user, token)
+        .then(() => setIsAuth(true))
+        .catch((error) => console.error('Failed to connect user', error));
+    }
+    // Disconnect the user when the component unmounts
+    return () => client.disconnectUser().catch((error) => console.error('Failed to disconnect user', error));
+  }, [token]);
 
   const logOut = () => {
-    cookies.remove("token");
-    cookies.remove("userId");
-    cookies.remove("firstName");
-    cookies.remove("lastName");
-    cookies.remove("username");
-    cookies.remove("hashedPassword");
+    clearUserCookies();
     client.disconnectUser();
     setIsAuth(false);
-    setOpenModal(false);
   };
-
-  if (token) {
-    client
-      .connectUser(
-        {
-          id: cookies.get("userId"),
-          name: cookies.get("username"),
-          firstName: cookies.get("firstName"),
-          lastName: cookies.get("lastName"),
-          hashedPassword: cookies.get("hashedPassword"),
-        },
-        token
-      )
-      .then(() => {
-        setIsAuth(true);
-      });
-  }
 
   return (
     <div className="App">
       <div className="title text-pop-up-top">Tic Tac Toe</div>
       {isAuth ? (
-        <Chat client={client}>
-          <JoinGame setIsAuth={setIsAuth} />
-          <div className="loggedInUser" onClick={handleOpenModal}>
-            {cookies.get("username")}
-          </div>
-          <Modal open={openModal} onClose={handleCloseModal}>
-            <Box className="modalContent">
-              <div>Are you sure you want to log out?</div>
-              <Button onClick={logOut}>Log Out</Button>
-            </Box>
-          </Modal>
-        </Chat>
+        <>
+          <div className="username-display">{user.name}</div>
+          <Chat client={client}>
+            <JoinGame />
+            <button onClick={logOut}>Log Out</button>
+          </Chat>
+        </>
       ) : (
         <div className="auth">
-          <div className="authForms">
+          <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "20px" }}>
             <SignUp setIsAuth={setIsAuth} />
             <Login setIsAuth={setIsAuth} />
           </div>
